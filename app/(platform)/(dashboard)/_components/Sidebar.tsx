e8591dc33c184811243e951bd0a4febf1d46d380
KVIKERITAS <aicamaurisk@gmail.com>
2023-11-24 23:37:33 +0200

    Added sidebar

---
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { Accordion } from '@/components/ui/accordion'
+import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useOrganization, useOrganizationList } from '@clerk/nextjs'
+import { Plus } from 'lucide-react'
+import Link from 'next/link'
+import { useLocalStorage } from 'usehooks-ts'
+import { NavItem, TOrganization } from './NavItem'
+
+type TSidebarProps = {
+	storageKey?: string
+}
+
+export const Sidebar = ({ storageKey = 't-sidebar-state' }: TSidebarProps) => {
+	const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+		storageKey,
+		{},
+	)
+
+	const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+		useOrganization()
+
+	const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+		userMemberships: { infinite: true },
+	})
+
+	const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+		(acc: string[], key: string) => {
+			if (expanded[key]) {
+				acc.push(key)
+			}
+			return acc
+		},
+		[],
+	)
+
+	const onExpand = (id: string) => {
+		setExpanded(curr => ({
+			...curr,
+			[id]: !expanded[id],
+		}))
+	}
+
+	if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+		return (
+			<>
+				<Skeleton />
+			</>
+		)
+	}
+
+	return (
+		<>
+			<div className='font-medium text-xs flex items-center mb-1'>
+				<span className='pl-4'>Workspaces</span>
+				<Button
+					className='ml-auto'
+					type='button'
+					size='icon'
+					variant='ghost'
+					asChild
+				>
+					<Link href='/select-org'>
+						<Plus className='h-4 w-4' />
+					</Link>
+				</Button>
+			</div>
+			<Accordion
+				type='multiple'
+				defaultValue={defaultAccordionValue}
+				className='space-y-2'
+			>
+				{userMemberships.data.map(({ organization }) => (
+					<NavItem
+						key={organization.id}
+						isActive={activeOrganization?.id === organization.id}
+						isExpanded={expanded[organization.id]}
+						organization={organization as TOrganization}
+						onExpand={onExpand}
+					/>
+				))}
+			</Accordion>
+		</>
+	)
+}
